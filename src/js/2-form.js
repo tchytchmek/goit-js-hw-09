1e35bbe64c4abee793874154eef1a77bce6fe8af
yaroslav228 <kibersportstandoff69@gmail.com>
2026-04-28 13:06:36 +0300

    form made, refresh and submit fixed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,22 +2,49 @@ const formData = {
     email: "",
     message: "",
 }
+
 const LS_KEY = "feedback-form-state";
+
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.email');
 const message = document.querySelector('.textarea');
 
-email.value = JSON.parse(localStorage.getItem(LS_KEY)).email;
-message.value = JSON.parse(localStorage.getItem(LS_KEY)).message;
-
 form.addEventListener('input', handleInput);
 form.addEventListener('submit', handleSubmit);
 
+populateForm();
+
+function populateForm(){
+  let putEmail;
+  let putMessage;
+  try{
+   putEmail = JSON.parse(localStorage.getItem(LS_KEY)).email;
+   putMessage = JSON.parse(localStorage.getItem(LS_KEY)).message;
+   }catch(e){
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+   }
+   if(putEmail){
+    email.value = putEmail;
+   }
+   if(putMessage){
+    message.value = putMessage;
+   }
+}
+
 function handleInput(event){
+
+  try{
+  formData.email= JSON.parse(localStorage.getItem(LS_KEY)).email;
+  formData.message = JSON.parse(localStorage.getItem(LS_KEY)).message;
+  }catch(e){
+      localStorage.setItem(LS_KEY, JSON.stringify(formData));
+
+  }
   if(event.target.classList.contains('email')){
     formData.email = event.target.value;
-  }else if(event.target.classList.contains('textarea')){
+  }
+  if(event.target.classList.contains('textarea')){
     formData.message = event.target.value;
   }
   localStorage.setItem(LS_KEY, JSON.stringify(formData));
@@ -25,5 +52,17 @@ function handleInput(event){
 
 function handleSubmit(event){
     event.preventDefault();
+    const elements = event.currentTarget.elements;
+    if(!elements.email || !elements.message){
+      alert('Fill please all fields');
+    }else{
+    localStorage.removeItem(LS_KEY);
+
+    formData.email = "";
+    formData.message = "";
+
+    event.currentTarget.reset();
+
+    }
 
 }
